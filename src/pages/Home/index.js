845ec92aae9942951267cf7b12b58a50fde2cd35
Roynard wei <yuishang@gmail.com>
2021-11-13 12:05:36 +0800

    ＃_1.2 規劃state & props (2021-11-13)
    #規劃state & props
    #setA +200 加法值會返回上次的數據

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import Edit from './components/Edit.js'
 import List from './components/List.js'
 import './index.css'
 
 
 const Home = () => {
+
+  // let a = 100
+  const [a, setA] = useState(100)
+  function plus(){
+    setA(function (prev) {
+      return prev + 200
+    })
+  }
   return <div className="app">
+    {a}
+    <button onClick={plus}>加法</button>
     <Edit />
     <List />
   </div>
